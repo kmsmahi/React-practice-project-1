1e589926bc24ca40c478c08f81e54913edfa71ea
kmsmahi <salauddinmahi2002@gmail.com>
2026-02-08 08:23:25 +0600

    first commit

---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const countries = () => {
+    return (
+        <div>
+            <h1>All countries are here</h1>
+        </div>
+    );
+};
+
+export default countries;
