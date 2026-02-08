b4ddcc5bb09da41063154e0907abf8d9c7496821
kmsmahi <salauddinmahi2002@gmail.com>
2026-02-08 20:59:07 +0600

    final commit

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,17 +1,30 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../country/country';
 import './countries.css';
 const Countries = ({countriesPromise}) => {
     const countriesData=use(countriesPromise);
     const countries=countriesData.countries;
-    
+    const[visitedCountries,setVisitedCountries]=useState([]);
+    const visitedCountriesHandler=(country)=>{
+        const newVisitedcountries=[...visitedCountries,country];
+        setVisitedCountries(newVisitedcountries);
+    }
     return (
         <div>
             <h1>All countries are here: {countries.length}</h1>
+            <h1>Total country visited: {visitedCountries.length}</h1>
+            <ol>
+                {
+                visitedCountries.map(country=>(
+                    <li key={country.cca3.cca3}>{country.name.common}</li>
+                ))
+            }
+            </ol>
             <div className='countries'>
                 {
                 countries.map(country=>(
-                    <Country  key={country.cca3} country={country}></Country>
+                    <Country  key={country.cca3.cca3} country={country} 
+                    visitedCountriesHandler={visitedCountriesHandler}></Country>
                 ))
             }
             </div>
--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css';
-const Country = ({country}) => {
-    
+const Country = ({country,visitedCountriesHandler}) => {
+    const[visited,setVisited]=useState(false);
+    const btnHandle=()=>{
+        if(visited){
+            setVisited(false);
+        }
+        else{
+            setVisited(true);
+        }
+        visitedCountriesHandler(country);
+    }
     return (
-        <div className='country'>
+        <div className={`country ${visited ? 'country-visited':''}`}>
             <img src={country.flags.flags.png} alt="" />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <button onClick={btnHandle} className='btn'>
+                {visited ? 'visited' : 'Not visited'}
+            </button>
         </div>
     );
 };
